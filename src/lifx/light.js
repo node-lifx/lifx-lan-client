d153f55179b750c48096720bc05746af56042ef6
Ristomatti Airo <ristomatti@kallio.io>
2017-10-28 21:23:55 +0300

    Require lodash functions directly

--- src/lifx/light.js
+++ src/lifx/light.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const {packet, constants, validate, utils} = require('../lifx');
-const _ = require('lodash');
+const {assign, pick} = require('lodash');
 
 /**
  * A representation of a light bulb
@@ -213,12 +213,12 @@ Light.prototype.getHardwareVersion = function(callback) {
     if (err) {
       return callback(err, null);
     }
-    const versionInfo = _.pick(msg, [
+    const versionInfo = pick(msg, [
       'vendorId',
       'productId',
       'version'
     ]);
-    callback(null, _.assign(
+    callback(null, assign(
       versionInfo,
       utils.getHardwareDetails(versionInfo.vendorId, versionInfo.productId)
     ));
@@ -273,7 +273,7 @@ Light.prototype.getFirmwareVersion = function(callback) {
     if (err) {
       return callback(err, null);
     }
-    callback(null, _.pick(msg, [
+    callback(null, pick(msg, [
       'majorVersion',
       'minorVersion'
     ]));
@@ -294,7 +294,7 @@ Light.prototype.getFirmwareInfo = function(callback) {
     if (err) {
       return callback(err, null);
     }
-    callback(null, _.pick(msg, [
+    callback(null, pick(msg, [
       'signal',
       'tx',
       'rx'
@@ -316,7 +316,7 @@ Light.prototype.getWifiInfo = function(callback) {
     if (err) {
       return callback(err, null);
     }
-    callback(null, _.pick(msg, [
+    callback(null, pick(msg, [
       'signal',
       'tx',
       'rx'
@@ -338,7 +338,7 @@ Light.prototype.getWifiVersion = function(callback) {
     if (err) {
       return callback(err, null);
     }
-    return callback(null, _.pick(msg, [
+    return callback(null, pick(msg, [
       'majorVersion',
       'minorVersion'
     ]));
