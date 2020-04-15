babee7c8dc97dabfc71e21e473fc11294475527c
Ristomatti Airo <ristomatti.airo@gmail.com>
2020-04-15 16:39:32 +0300

    Use light.setMultiZoneEffect instead of sending a raw packet

--- example/setMultiZoneEffect.js
+++ example/setMultiZoneEffect.js
@@ -5,16 +5,7 @@
  */
 
 const LifxClient = require('../lib/lifx').Client;
-const packet = require('../lib/lifx').packet;
-const constants = require('../lib/lifx').constants;
 const client = new LifxClient();
-
-// Create our packet with for move effect - runs infinite
-const packetObj = packet.create('setMultiZoneEffect', {
-  effectType: constants.MULTIZONE_EFFECTS.indexOf('MOVE'), // MOVE or OFF
-  speed: 1000, // 1s - speed of one full animation cycle
-  parameter2: constants.MULTIZONE_EFFECTS_MOVE_DIRECTION.indexOf('AWAY') // TOWARDS or AWAY from controller
-}, client.source);
 
 // Function running when packet was received by light
 const onPacketReceived = () => {
@@ -25,9 +16,7 @@ client.on('light-new', (light) => {
   console.log('New light found.');
   console.log('ID: ' + light.id);
 
-  // Set the light id
-  packetObj.target = light.id; // set target id to new light
-  client.send(packetObj, onPacketReceived);
+  light.setMultiZoneEffect('OFF', 1000, 'AWAY', onPacketReceived);
 });
 
 // Give feedback when running
